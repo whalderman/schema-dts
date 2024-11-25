4c51b46d056c458a645bcd2ffd20a06fd21b600f
Warren Halderman <halderman.warren@gmail.com>
2024-11-25 22:06:18 +0900

    use built-in fetch, squoosh the filter fn

--- packages/schema-dts-gen/src/triples/reader.ts
+++ packages/schema-dts-gen/src/triples/reader.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2023 Google LLC
+ * Copyright 2023 Google LLC, 2024 Warren Halderman
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,13 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import https from 'https';
 import fs from 'fs/promises';
 
-import {Log} from '../logging/index.js';
-
-import {Parser, Store} from 'n3';
-import type {Quad} from 'n3';
+import { Parser, Store } from 'n3';
+import type { Quad } from 'n3';
 
 function asQuads(data: string): Quad[] {
   return new Parser({}).parse(data);
@@ -42,71 +39,22 @@ export async function loadFile(path: string): Promise<Store> {
 }
 
 async function handleFile(path: string): Promise<Quad[]> {
-  const fileStr = await fs.readFile(path, {encoding: 'utf8'});
+  const fileStr = await fs.readFile(path, { encoding: 'utf8' });
   return asQuads(fileStr);
 }
 
-function handleUrl(url: string): Promise<Quad[]> {
-  return new Promise<Quad[]>((resolve, reject) => {
-    https
-      .get(url, response => {
-        Log(`Got Response ${response.statusCode}: ${response.statusMessage}.`);
-        if (response.statusCode !== 200) {
-          const location =
-            response.headers['location'] ||
-            response.headers['content-location'];
-
-          if (location) {
-            Log(`Handling redirect to ${location}...`);
-            resolve(handleUrl(location));
-            return;
-          }
-
-          reject(
-            new Error(
-              `Got Errored Response ${response.statusCode}: ${response.statusMessage}.`
-            )
-          );
-          return;
-        }
-
-        const data: string[] = [];
-
-        response.on('data', (chunkB: Buffer) => {
-          const chunk = chunkB.toString('utf-8');
-          data.push(chunk);
-        });
-
-        response.on('end', () => {
-          try {
-            resolve(asQuads(data.join('')));
-          } catch (error) {
-            Log(`Caught Error on end: ${error}`);
-            reject(error);
-          }
-        });
-
-        response.on('error', error => {
-          Log(`Saw error: ${error}`);
-          reject(error);
-        });
-      })
-      .on('error', reject);
-  });
+async function handleUrl(url: string): Promise<Quad[]> {
+  const res = await fetch(url);
+  const text = await res.text();
+  return asQuads(text);
 }
 
 export function process(quads: Quad[]): Store {
-  const filtered = quads.filter(quad => {
-    if (
-      quad.subject.termType === 'NamedNode' &&
-      quad.subject.value.includes('file:///')
-    ) {
-      // Inexplicably, local files end up in the public schema for
-      // certain layer overlays.
-      return false;
-    }
-
-    return true;
-  });
+  // Inexplicably, local files end up in the public schema for
+  // certain layer overlays. (?)
+  const filtered = quads.filter((quad) => !(
+    quad.subject.termType === 'NamedNode' &&
+    quad.subject.value.includes('file:///')
+  ));
   return new Store(filtered);
 }
